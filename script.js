ab779a0347244ef042f42b7790974e7cb3e1d023
Sai Kiran P <pvkc22@gmail.com>
2025-04-02 21:57:22 +0530

    added code

---
+++ script.js
@@ -0,0 +1,9 @@
+/* code for dynamic header and footer */
+function loadComponent(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => document.getElementById(elementId).innerHTML = data);
+}
+
+loadComponent("Header", "Header.html");
+loadComponent("Footer", "Footer.html");
